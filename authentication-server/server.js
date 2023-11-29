12af8b2776699febcae6715eb66f906536b59c4f
eng0waleed <eng.0waleed@gmail.com>
2023-11-30 02:12:11 +0300

    added server.js

---
+++ authentication-server/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const  { connectToDB, getDB } = require('./db.js');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json());
+
+let db;
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        // Create a new user with the additional fields from the form
+        const user = { 
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            phone: req.body.phone,
+            age: req.body.age,
+            incomeSource: req.body.incomeSource // make sure to match this field name with your form input name
+        };
+
+        await db.collection('users').insertOne(user);
+
+        res.status(201).send('User registered');
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message);
+    }
+});
+
+// New endpoint to get all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await db.collection('users').find({}).toArray();
+        res.json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const user = await db.collection('users').findOne({ username: req.body.username });
+
+        if (!user) {
+            return res.status(400).send('Cannot find user');
+        }
+
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+            res.json({ accessToken: accessToken });
+        } else {
+            res.send('Not Allowed');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+connectToDB(process.env.MONGODB_URI).then(() => {
+    db = getDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
--- graphql-service/models/Transaction.js
+++ graphql-service/models/Transaction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TransactionSchema = new mongoose.Schema({
-  // userId: {
+  // user: {
   //   type: Schema.Types.ObjectId,
   //   ref: 'User',
   //   required: true
