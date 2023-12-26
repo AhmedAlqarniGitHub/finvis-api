0400359afbb175d6919010d278c80cabeaa782d8
eng0waleed <eng.0waleed@gmail.com>
2023-12-26 21:30:40 +0300

    updated

--- graphql-service/resolvers.js
+++ graphql-service/resolvers.js
@@ -6,7 +6,7 @@ const Transaction = require('./models/Transaction')
 const resolvers = {
   Query: {
     user: async (_, { id }) => {
-      const user = await User.findById(id).populate('obligations');
+      const user = await User.findById(id).populate("obligations");
       console.log(user); // Check the output
       return user;
     },
@@ -22,32 +22,32 @@ const resolvers = {
     updateUser: async (_, { userId, salary, salaryDay, obligations }) => {
       // First, handle the obligations
       await Obligation.deleteMany({ user: userId });
-  
+
       const newObligations = await Obligation.insertMany(
-        obligations.map(obligation => ({
+        obligations.map((obligation) => ({
           ...obligation,
-          user: userId
+          user: userId,
         }))
       );
-  
+
       // Get the IDs of the new obligations
-      const obligationsIds = newObligations.map(obligation => obligation._id);
-  
+      const obligationsIds = newObligations.map((obligation) => obligation._id);
+
       // Now, update the user with the new salary, salaryDay, and obligations
       const updatedUser = await User.findByIdAndUpdate(
         userId,
-        { 
-          salary, 
-          salaryDay, 
-          obligations: obligationsIds 
+        {
+          salary,
+          salaryDay,
+          obligations: obligationsIds,
         },
         { new: true }
-      ).populate('obligations');
-  
+      ).populate("obligations");
+
       if (!updatedUser) {
-        throw new Error('User not found');
+        throw new Error("User not found");
       }
-  
+
       return updatedUser;
     },
     addSavingsTarget: async (_, { userId, targetAmount }) => {
@@ -56,7 +56,11 @@ const resolvers = {
       if (existingTarget) {
         const updatedTarget = await SavingsTarget.findOneAndUpdate(
           { user: userId },
-          { targetAmount },
+          {
+            user: userId,
+            targetAmount,
+            currentAmount: await calculateTotalSavings(userId),
+          },
           { new: true }
         );
         return updatedTarget;
@@ -64,11 +68,28 @@ const resolvers = {
         const newTarget = new SavingsTarget({
           user: userId,
           targetAmount,
-          currentAmount: 0
+          currentAmount: await calculateTotalSavings(userId),
         });
         await newTarget.save();
         return newTarget;
       }
+    },
+    addTransaction: async (
+      _,
+      { userId, amount, date, transactionType, description, category }
+    ) => {
+      const newTransaction = new Transaction({
+        user: userId,
+        amount,
+        date,
+        transactionType,
+        description,
+        category,
+      });
+
+      await newTransaction.save();
+
+      return newTransaction;
     },
     // ... other mutation resolvers ...
   },
